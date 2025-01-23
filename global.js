1ec547d7c8a8c8bd79314d90b5980da39195578a
emilyyiip <144075069+emilyyiip@users.noreply.github.com>
2025-01-23 13:11:32 -0800

    js updated

--- global.js
+++ global.js
@@ -15,3 +15,19 @@ const currentLink = navLinks.find(
 
 // Add the 'current' class to the current page link
 currentLink?.classList.add('current');
+
+let pages = [
+  { url: 'index.html', title: 'Home' },
+  { url: 'projects/index.html', title: 'Projects' },
+  { url: 'contact/index.html', title: 'Contact' },
+  { url: 'resume/index.html', title: 'Resume' },
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+}
